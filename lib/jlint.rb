5557574d30460c585468a6259c303fd6b31186f1
soffolk <zlx.star@gmail.com>
2014-10-20 15:46:15 +0800

    update lint to return array contain message

--- lib/jlint.rb
+++ lib/jlint.rb
@@ -1,6 +1,30 @@
 class Jlint
+  def self.lint file_path, config
+    new.lint file_path, config
+  end
+
+  def initialize
+  end
+
   def lint file_path, config
-    # system("java -jar ~/code/mine/jlint/checkstyle.jar -c ~/code/mine/jlint/doc/sun_checks.xml ~/Documents/AnimationFunctions.java")
-    results = system("java -jar #{../checkstyle.jar} -c #{../doc/sun_checks.xml} #{file_path}")
+    parse `java -jar #{checkstyle_command} -c #{checkstyle_config} #{file_path}`
+  end
+
+  private
+  def checkstyle_command
+    File.join(File.dirname(__FILE__), '..', 'bin', 'checkstyle.jar')
+  end
+
+  def checkstyle_config
+    File.join(File.dirname(__FILE__), '..', 'doc', 'sun_checks.xml')
+  end
+
+  def parse results
+    array = results.split("\n")
+    array.reject! { |s| s == 'Starting audit...' || s == 'Audit done.' }
+    array.map! do |msg|
+      msgs = msg.split(":")
+      [msgs.last.strip, msgs[1]]
+    end
   end
 end

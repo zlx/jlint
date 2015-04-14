e15874c8bf0ce92d9ebe3116a91f8c0d98d618b2
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2015-04-14 16:15:18 -0400

    added error handling for custom configuration

--- test/jlint/test_jlint.rb
+++ test/jlint/test_jlint.rb
@@ -20,11 +20,34 @@ describe Jlint do
 }]
     result = Jlint.lint(content)
     assert result.is_a?(Array)
-    assert !result.include?(["File does not end with a newline.", 0])
     assert_equal(result.to_yaml, File.read('test/support/custom_config_errors.yml'))
   end
 
   it "should lint with custom config content" do
+    content = %Q[import java.util.*;
+public class Sample {
+  public static void Sample() {
+  }
+
+  public void hi(long name) {
+    system.out.printf("Hi, " + name);
+  }
+}]
+    config_content = %Q[<?xml version="1.0"?>
+<!DOCTYPE module PUBLIC
+          "-//Puppy Crawl//DTD Check Configuration 1.3//EN"
+          "http://www.puppycrawl.com/dtds/configuration_1_3.dtd">
+<module name="Checker">
+  <module name="TreeWalker">
+    <module name="AvoidStarImport"/>
+  </module>
+</module>]
+    result = Jlint.new(config_content).lint(content)
+    assert result.is_a?(Array)
+    assert result.include?(["Using the '.*' form of import should be avoided - java.util.*.", 1])
+  end
+
+  it "should error on bad custom config content" do
     content = %Q[public class Sample {
   public static void Sample() {
   }
@@ -38,10 +61,28 @@ describe Jlint do
           "-//Puppy Crawl//DTD Check Configuration 1.3//EN"
           "http://www.puppycrawl.com/dtds/configuration_1_3.dtd">
 <module name="Checker">
+  <module name="AvoidStarImport"/>
 </module>]
-    result = Jlint.new(config_content).lint(content)
-    assert result.is_a?(Array)
-    assert !result.include?(["Missing package-info.java file.", 0])
+    assert_raises(RuntimeError) { Jlint.new(config_content).lint(content) }
+  end
+
+  it "should error on bad XML" do
+    content = %Q[public class Sample {
+  public static void Sample() {
+  }
+
+  public void hi(long name) {
+    system.out.printf("Hi, " + name);
+  }
+}]
+    config_content = %Q[<?xml version="1.0"?>
+<!DOCTYPE module PUBLIC
+          "-//Puppy Crawl//DTD Check Configuration 1.3//EN"
+          "http://www.puppycrawl.com/dtds/configuration_1_3.dtd">
+<module name="Checker">
+  <module name="AvoidStarImport"/>
+</module]
+    assert_raises(RuntimeError) { Jlint.new(config_content).lint(content) }
   end
 
 end
